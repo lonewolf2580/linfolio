ed12001fed84210a12277f86f7bfe522235889c0
Linus Ngwee <linusngwee@yahoo.com>
2023-08-10 16:19:07 +0100

    DevFolio

---
+++ portfolio.ts
@@ -0,0 +1,321 @@
+import emoji from 'react-easy-emoji';
+import {
+  EducationType,
+  ExperienceType,
+  FeedbackType,
+  ProjectType,
+  SkillsSectionType,
+  SkillBarsType,
+  SEODataType,
+  SocialLinksType,
+  GreetingsType,
+} from './types/sections';
+
+export const greetings: GreetingsType = {
+  name: 'Linus Ngwee',
+  title: "Hi all, I'm Lincom",
+  description:
+    "Experienced Fullstack Developer skilled in crafting dynamic web applications using technologies such as Node.js, Next.js, and Tailwind CSS. Proficient in GraphQL and MongoDB for efficient data management, with a strong background in PHP and MySQL. Passionate about building Progressive Web Apps (PWAs) and familiar with Strapi CMS for content management. Equipped with expertise in PostgreSQL and experienced in integrating AI capabilities through OpenAI. Excited to contribute to innovative projects and leverage LangChain for cutting-edge solutions. Blockchain development on Ethereum, Solidity, Web3.js, and Lisk.",
+  resumeLink:
+    'https://drive.google.com/file/d/1NiC1Q6unVtulLk5pSKK9fn_OtFGyF8Nn/view?usp=drivesdk',
+};
+
+export const openSource = {
+  githubUserName: 'lonewolf2580',
+};
+
+export const contact = {};
+
+export const socialLinks: SocialLinksType = {
+  url: 'https://lincomfolio.netlify.app/',
+  linkedin: 'https://www.linkedin.com/in/linus-ngwee-aa7366177',
+  github: 'https://github.com/lonewolf2580',
+  instagram: 'https://www.instagram.com/donlincom',
+  facebook: 'https://www.facebook.com/profile.php?id=100091903893933',
+  twitter: 'https://twitter.com/1hanzla100',
+};
+
+export const skillsSection: SkillsSectionType = {
+  title: 'What I do',
+  subTitle: 'Over the past 8 years, I\'ve explored so many areas of Tech, below are the areas I chose to specialize:',
+  data: [
+    {
+      title: 'Full Stack Development',
+      lottieAnimationFile: '/lottie/skills/fullstack.json', // Path of Lottie Animation JSON File
+      skills: [
+        emoji(
+          '⚡ Building responsive Single-Page-Apps (SPA) & PWA in React.js'
+        ),
+        emoji('⚡ Building responsive static websites using Next.js'),
+        emoji('⚡ Building RESTful APIs in Node.js & PHP'),
+      ],
+      softwareSkills: [
+        {
+          skillName: 'HTML-5',
+          iconifyTag: 'vscode-icons:file-type-html',
+        },
+        {
+          skillName: 'CSS-3',
+          iconifyTag: 'vscode-icons:file-type-css',
+        },
+        {
+          skillName: 'JavaScript',
+          iconifyTag: 'logos:javascript',
+        },
+        {
+          skillName: 'Reactjs',
+          iconifyTag: 'vscode-icons:file-type-reactjs',
+        },
+        {
+          skillName: 'Nextjs',
+          iconifyTag: 'vscode-icons:file-type-light-next',
+        },
+        {
+          skillName: 'PHP',
+          iconifyTag: 'logos:php',
+        },
+        {
+          skillName: 'Nodejs',
+          iconifyTag: 'vscode-icons:file-type-node',
+        },
+
+        {
+          skillName: 'Redux',
+          iconifyTag: 'logos:redux',
+        },
+        {
+          skillName: 'NPM',
+          iconifyTag: 'logos:npm-icon',
+        },
+        {
+          skillName: 'Yarn',
+          iconifyTag: 'logos:yarn',
+        },
+      ],
+    },
+    {
+      title: 'Cloud Infra-Architecture',
+      lottieAnimationFile: '/lottie/skills/cloudinfra.json', // Path of Lottie Animation JSON File
+      skills: [
+        emoji('⚡ Experience of working on multiple cloud platforms'),
+        emoji(
+          '⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases'
+        ),
+        emoji(
+          '⚡ Building CI/CD pipelines for automated testing & deployment using Github Actions'
+        ),
+      ],
+      softwareSkills: [
+        // ? Check README To get icon details
+        {
+          skillName: 'AWS',
+          iconifyTag: 'logos:aws',
+        },
+        {
+          skillName: 'Azure',
+          iconifyTag: 'logos:microsoft-azure',
+        },
+        {
+          skillName: 'Heroku',
+          iconifyTag: 'logos:heroku-icon',
+        },
+        {
+          skillName: 'PostgreSQL',
+          iconifyTag: 'logos:postgresql',
+        },
+        {
+          skillName: 'Github',
+          iconifyTag: 'akar-icons:github-fill',
+        },
+        {
+          skillName: 'Docker',
+          iconifyTag: 'logos:docker-icon',
+        },
+        {
+          skillName: 'Github Actions',
+          iconifyTag: 'logos:github-actions',
+        },
+        {
+          skillName: 'Cloudinary',
+          iconifyTag: 'logos:cloudinary',
+        },
+        {
+          skillName: 'Nginx',
+          iconifyTag: 'logos:nginx',
+        },
+        {
+          skillName: 'Sentry',
+          iconifyTag: 'logos:sentry-icon',
+        },
+      ],
+    },
+    {
+      title: 'Blockchain',
+      lottieAnimationFile: '/lottie/skills/ethereum.json', // Path of Lottie Animation JSON File
+      skills: [
+        emoji(
+          '⚡ Experience in developing Smart Contract using Solidity & Ethereum'
+        ),
+        emoji(
+          '⚡ Building Scripts for automated testing & deployment of Smart Contracts using Brownie & Infura'
+        ),
+        emoji(
+          '⚡ Experience of using Openzeppelin Smart Contract Standards & Chainlink Oracles'
+        ),
+        emoji('⚡ Developing NFT Smart Contracts using ERC-721 Token Standard'),
+        emoji(
+          '⚡ Building Dapps with React.js & Solidity using Web3.js, Moralis & IPFS'
+        ),
+      ],
+      softwareSkills: [
+        {
+          skillName: 'Ethereum',
+          iconifyTag: 'logos:ethereum',
+        },
+        {
+          skillName: 'Solidity',
+          iconifyTag: 'logos:solidity',
+        },
+        {
+          skillName: 'Web3js',
+          iconifyTag: 'logos:web3js',
+        },
+        {
+          skillName: 'Metamask',
+          iconifyTag: 'logos:metamask-icon',
+        },
+        {
+          skillName: 'Ganache',
+          iconifyTag: 'logos:ganache-icon',
+        },
+      ],
+    },
+  ],
+};
+
+export const SkillBars: SkillBarsType[] = [
+  {
+    Stack: 'Frontend/Design', //Insert stack or technology you have experience in
+    progressPercentage: '90', //Insert relative proficiency in percentage
+  },
+  {
+    Stack: 'Backend',
+    progressPercentage: '100',
+  },
+  {
+    Stack: 'AI',
+    progressPercentage: '70',
+  },
+];
+
+export const educationInfo: EducationType[] = [
+  {
+    schoolName: 'Aptech Computer Education',
+    subHeader: 'Advanced Diploma in Software Engineering',
+    duration: 'January 2019 - March 2021',
+    desc: 'Was exposed to various Fields of Technology listed below:',
+    grade: 'Grade A',
+    descBullets: [
+      'UI Design',
+      'Software Engineering',
+      'Networkng',
+      'Server Administration',
+      'Embedded Systems',
+      'Project Documentation Writing',
+    ],
+  },
+];
+
+export const experience: ExperienceType[] = [
+  {
+    role: 'Frontend Developer',
+    company: 'Duseca Software',
+    companyLogo: '/img/icons/common/dusecaSoftware.jpg',
+    date: 'Apr 2022 – Jun 2022',
+    desc: 'I worked as frontend web developer to design web based admin dashboards for mulitple apps using Django, Bootstrap, jQuery, Chart.js and some other libraries.',
+  },
+  {
+    role: 'API Engineer',
+    company: 'Duseca Software',
+    companyLogo: '/img/icons/common/dusecaSoftware.jpg',
+    date: 'Jan 2022 – Mar 2022',
+    desc: 'I worked as API Engineer on a Kale App. Kale is a community platform for the creation of social reviews based on verified transactions. I built Fully Optimized and High Performance RESTful APIs using Django, DjangoRESTFramework, PostgresSQL, JWT Authentication, AWS, Integrated Plaid APIs for Fetching Bank Transactions, Integrated Sentry for monitoring Django Server and deployed the API on Heroku Server.',
+    // descBullets: [
+    // 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+    // 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    // ],
+  },
+  {
+    role: 'Full Stack Developer',
+    company: 'Bleed-AI',
+    companyLogo: '/img/icons/common/bleedAI.jpg',
+    date: 'Sept 2021 - Oct 2021',
+    desc: 'Worked as django web developer to create a website in which I integrated computer vision AI model (built by the team of BleedAI) to process YouTube URLs and show Real Time processing on Website using Django Channels WebSockets , threading for running multiple instances of AI Model, jQuery for UI rendering and deployed it on Heroku server.',
+  },
+  {
+    role: 'Backend Developer',
+    company: 'Wapidu',
+    companyLogo: '/img/icons/common/wapidu.jpg',
+    date: 'Sept 2021',
+    desc: 'Worked as a Django Developer to integrate Stripe payment gateway in wapidu.com and Created REST APIs using Django REST Framework to integrate Django Stripe backend to Vue.js Frontend and Deployed it on Azure based Docker container registry.',
+  },
+];
+
+export const projects: ProjectType[] = [
+  {
+    name: 'developer-portfolio',
+    desc: 'Software Developer Portfolio Template built with react.js and next.js bootstrap that helps you showcase your work and skills as a software developer.',
+    github: 'https://github.com/1hanzla100/developer-portfolio',
+    link: 'https://developer-portfolio-1hanzla100.vercel.app/',
+  },
+  {
+    name: 'AtlasMart',
+    desc: 'With Atlas Mart, it’s easy to find the products with the best price and ship them straight to your door.',
+    github: 'https://github.com/1hanzla100/Django-React-Marketplace',
+  },
+  {
+    name: 'Technota (Forum)',
+    desc: 'Get hands-on experience in technical skills with Technota',
+    github: 'https://github.com/1hanzla100/django-react-forum',
+  },
+  {
+    name: 'Shopaza (Ecommerce)',
+    desc: 'Now your reach to all your favorite entertainment and luxury items is just one touch apart',
+    github: 'https://github.com/1hanzla100/Django-ecommerce',
+  },
+];
+
+export const feedbacks: FeedbackType[] = [
+  {
+    name: 'John Smith',
+    feedback:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
+  },
+  {
+    name: 'John Smith',
+    feedback:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
+  },
+];
+
+// option to hide or show the ContactUs component
+export const showContactUs: boolean = true;
+
+// See object prototype on /types/section.ts page
+export const seoData: SEODataType = {
+  title: 'Linus Ngwee',
+  description:
+    'A passionate Full Stack Web Developer and Blockchain Developer.',
+  author: 'Linus Ngwee',
+  image: 'https://avatars3.githubusercontent.com/u/59178380?v=4',
+  url: 'https://developer-portfolio-1hanzla100.vercel.app',
+  keywords: [
+    'Linus',
+    'Linus Ngwee',
+    'lonewolf2580',
+    'Lincom',
+    'Portfolio',
+    'Linus Portfolio ',
+    'Linus Ngwee Portfolio',
+  ],
+};
